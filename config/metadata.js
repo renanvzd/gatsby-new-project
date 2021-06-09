759e2d5642a41ba294d9f1697d1de4bd58abc18d
Renan Veronez Drechsler <renanvz@outlook.com>
2021-06-09 12:35:28 -0300

    First commit

---
+++ config/metadata.js
@@ -0,0 +1,11 @@
+module.exports = {
+  title: `Moradas do Pé Grande`,
+    siteUrl: `https://www.moradasdopegrande.com.br`,
+    description: `Hospedagens em Tramandaí/RS`,
+    social: [
+      {
+        name: 'Facebook',
+        url: 'https://facebook.com/moradasdopegrande'
+      }
+    ]
+}
--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,84 @@
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
 
+const siteMetadata = require('./config/metadata');
+
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  siteMetadata, 
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sitemap`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `MoradasDoPeGrande`,
+        short_name: `PeGrande`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+        icon: `src/assets/icon.png`, // 512 x 512 coloca o favicon
+      },
+    },
+    {
+      resolve: `gatsby-plugin-nprogress`,
+      options: {
+        color: `#7159c1`,
+        // Disable the loading spinner.
+        showSpinner: false,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "YOUR_GOOGLE_ANALYTICS_TRACKING_ID",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+        // Enables Google Optimize using your container Id
+        optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
+        // Enables Google Optimize Experiment ID
+        experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
+        // Set Variation ID. 0 for original 1,2,3....
+        variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
+        // Defers execution of google analytics script after page load
+        defer: false,
+        // Any additional optional fields
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        cookieDomain: "example.com",
+      },
+    },
+    {
+      resolve: `gatsby-plugin-canonical-urls`,
+      options: {
+        siteUrl: `https://www.moradasdopegrande.com.br`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        displayName: process.env.NODE_ENV !== 'production',
+      },
+    },
+    {
+      resolve: `gatsby-plugin-page-creator`,
+      options: {
+        path: `${__dirname}/src/pages`,
+        ignore: [`**/styles.js`],
+      },
+    },
+    `gatsby-plugin-offline`
+  ]
 }
