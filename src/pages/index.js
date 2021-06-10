1190a266dce34f83295990b880c48deecd393245
Renan Veronez Drechsler <renanvz@outlook.com>
2021-06-10 19:33:44 -0300

    Structure

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Layout from '../components/Layout/Layout';
+import "./home.css"
 
 export default function Home() {
   return (
     <Layout>
-    <section>
+    <section className="header">
       <div>
         <h2>Design</h2> 
         <h3>Develop & Deploy</h3>
